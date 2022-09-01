2b9e86c9f64aceebddf57330639a01a54f24b616
Dominik Lewczyński <dominiklew200060@gmail.com>
2022-09-01 15:57:38 +0200

    Add new js file that allow slider bettwen slides

---
+++ js/slider.js
@@ -0,0 +1,46 @@
+let slideIndex = 1;
+showSlides(slideIndex);
+
+function currentSlide(n) {
+    showSlides(slideIndex = n);
+}
+
+function showSlides(n) {
+    let i;
+    let slides = document.getElementsByClassName("benefits__slider-slide");
+    let dots = document.getElementsByClassName("benefits__buttons-item");
+    if (n > slides.length) { slideIndex = 1 }
+    if (n < 1) { slideIndex = slides.length }
+    for (i = 0; i < slides.length; i++) {
+        slides[i].style.display = "none";
+    }
+    for (i = 0; i < dots.length; i++) {
+        dots[i].className = dots[i].className.replace(" active", "");
+    }
+    slides[slideIndex - 1].style.display = "block";
+    dots[slideIndex - 1].className += " active";
+}
+
+
+let slideIndex2 = 1;
+showSlides2(slideIndex2);
+
+function currentSlide2(n) {
+    showSlides2(slideIndex2 = n);
+}
+
+function showSlides2(n) {
+    let i;
+    let slides = document.getElementsByClassName("references__slider-slide");
+    let dots = document.getElementsByClassName("references__buttons-item");
+    if (n > slides.length) { slideIndex = 1 }
+    if (n < 1) { slideIndex2 = slides.length }
+    for (i = 0; i < slides.length; i++) {
+        slides[i].style.display = "none";
+    }
+    for (i = 0; i < dots.length; i++) {
+        dots[i].className = dots[i].className.replace(" active", "");
+    }
+    slides[slideIndex2 - 1].style.display = "block";
+    dots[slideIndex2 - 1].className += " active";
+}
