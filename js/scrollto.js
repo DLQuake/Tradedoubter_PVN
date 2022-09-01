2aa149305c75cb3bd646e6972877711de4e29bfc
Dominik Lewczyński <dominiklew200060@gmail.com>
2022-09-01 15:57:15 +0200

    Add new id's to scroll to

--- js/scrollto.js
+++ js/scrollto.js
@@ -1,6 +1,7 @@
 jQuery(function ($) {
     $.scrollTo(0);
     $('#feature').click(function () { $.scrollTo($('#Tofeature'), 1000); });
+    $('#feature0').click(function () { $.scrollTo($('#Tofeature'), 1000); });
 
     $('#feature1').click(function () { $.scrollTo($('#Tofeature1'), 1000); });
     $('#feature2').click(function () { $.scrollTo($('#Tofeature2'), 1000); });
@@ -8,5 +9,15 @@ jQuery(function ($) {
     $('#feature4').click(function () { $.scrollTo($('#Tofeature4'), 1000); });
     $('#feature5').click(function () { $.scrollTo($('#Tofeature5'), 1000); });
     $('#feature6').click(function () { $.scrollTo($('#Tofeature6'), 1000); });
+
+    $('#aboutus').click(function () { $.scrollTo($('#Toaboutus'), 1000); });
+    $('#aboutus1').click(function () { $.scrollTo($('#Toaboutus'), 1000); });
+
+    $('#references').click(function () { $.scrollTo($('#Toreferences'), 1000); });
+    $('#references1').click(function () { $.scrollTo($('#Toreferences'), 1000); });
+    $('#client').click(function () { $.scrollTo($('#Toreferences'), 1000); });
+
+    $('#contact').click(function () { $.scrollTo($('#Tocontact'), 1000); });
+    $('#contact1').click(function () { $.scrollTo($('#Tocontact'), 1000); });
 }
 );
