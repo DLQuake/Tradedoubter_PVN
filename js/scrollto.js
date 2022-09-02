7e598471afe73426e88a9a573db4294ed33fb2c0
Dominik Lewczyński <dominiklew200060@gmail.com>
2022-09-02 15:55:33 +0200

    change time of click

--- js/scrollto.js
+++ js/scrollto.js
@@ -1,23 +1,23 @@
 jQuery(function ($) {
     $.scrollTo(0);
-    $('#feature').click(function () { $.scrollTo($('#Tofeature'), 1000); });
-    $('#feature0').click(function () { $.scrollTo($('#Tofeature'), 1000); });
+    $('#feature').click(function () { $.scrollTo($('#Tofeature'), 200); });
+    $('#feature0').click(function () { $.scrollTo($('#Tofeature'), 200); });
 
-    $('#feature1').click(function () { $.scrollTo($('#Tofeature1'), 1000); });
-    $('#feature2').click(function () { $.scrollTo($('#Tofeature2'), 1000); });
-    $('#feature3').click(function () { $.scrollTo($('#Tofeature3'), 1000); });
-    $('#feature4').click(function () { $.scrollTo($('#Tofeature4'), 1000); });
-    $('#feature5').click(function () { $.scrollTo($('#Tofeature5'), 1000); });
-    $('#feature6').click(function () { $.scrollTo($('#Tofeature6'), 1000); });
+    $('#feature1').click(function () { $.scrollTo($('#Tofeature1'), 200); });
+    $('#feature2').click(function () { $.scrollTo($('#Tofeature2'), 200); });
+    $('#feature3').click(function () { $.scrollTo($('#Tofeature3'), 200); });
+    $('#feature4').click(function () { $.scrollTo($('#Tofeature4'), 200); });
+    $('#feature5').click(function () { $.scrollTo($('#Tofeature5'), 200); });
+    $('#feature6').click(function () { $.scrollTo($('#Tofeature6'), 200); });
 
-    $('#aboutus').click(function () { $.scrollTo($('#Toaboutus'), 1000); });
-    $('#aboutus1').click(function () { $.scrollTo($('#Toaboutus'), 1000); });
+    $('#aboutus').click(function () { $.scrollTo($('#Toaboutus'), 200); });
+    $('#aboutus1').click(function () { $.scrollTo($('#Toaboutus'), 200); });
 
-    $('#references').click(function () { $.scrollTo($('#Toreferences'), 1000); });
-    $('#references1').click(function () { $.scrollTo($('#Toreferences'), 1000); });
-    $('#client').click(function () { $.scrollTo($('#Toreferences'), 1000); });
+    $('#references').click(function () { $.scrollTo($('#Toreferences'), 200); });
+    $('#references1').click(function () { $.scrollTo($('#Toreferences'), 200); });
+    $('#client').click(function () { $.scrollTo($('#Toreferences'), 200); });
 
-    $('#contact').click(function () { $.scrollTo($('#Tocontact'), 1000); });
-    $('#contact1').click(function () { $.scrollTo($('#Tocontact'), 1000); });
+    $('#contact').click(function () { $.scrollTo($('#Tocontact'), 200); });
+    $('#contact1').click(function () { $.scrollTo($('#Tocontact'), 200); });
 }
 );
