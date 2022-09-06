a590fd1bc8837df6ccb30be767eee58ef835affc
Dominik Lewczyński <dominiklew200060@gmail.com>
2022-09-06 13:34:14 +0200

    Update swiper.js

--- js/swiper.js
+++ js/swiper.js
@@ -1,5 +1,22 @@
-const swiper = new Swiper('.swiper', {
+const swiper1 = new Swiper('.benefits__slider', {
     slidesPerView: 4,
+    spaceBetween: 30,
+    loop: true,
+    loopFillGroupWithBlank: true,
+    mousewheel: true,
+
+    pagination: {
+        el: '.swiper-pagination',
+        clickable: true,
+        dynamicBullets: true,
+        renderBullet: function (index, className) {
+            return '<span class="' + className + '">' + "</span>";
+        },
+    },
+});
+
+const swiper2 = new Swiper('.references__slider', {
+    slidesPerView: 2,
     spaceBetween: 30,
     loop: true,
     loopFillGroupWithBlank: true,
